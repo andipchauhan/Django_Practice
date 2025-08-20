8204b0f59355bff23d31f34a04c417edb089f4ea
Andip Chauhan <chauhan2244andip@gmail.com>
2025-08-20 12:27:52 +0530

    ModelForms and it's features

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,11 +1,31 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(label="Enter your name", max_length=100, error_messages={
-        "required": "Bhai naam to daalo",
-        "max_length": "only add Chinna Swami of Chinna swami mutuswami vengopal iyer..."
-    })
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(label="Enter your name", max_length=100, error_messages={
+#         "required": "Bhai naam to daalo",
+#         "max_length": "only add Chinna Swami of Chinna swami mutuswami vengopal iyer..."
+#     })
     
-    review_text = forms.CharField(label="Enter your review", widget=forms.Textarea, max_length=500)
+#     review_text = forms.CharField(label="Enter your review", widget=forms.Textarea, max_length=500)
 
-    rating = forms.IntegerField(label="Enter your rating", min_value=1, max_value=5)
+#     rating = forms.IntegerField(label="Enter your rating", min_value=1, max_value=5)
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        # fields = ['user_name', 'review_text', 'rating']
+        # fields = __all__
+        labels = {
+            'username': 'Your Name',
+            'review_text': 'Your Review',
+            'rating': 'Your Rating'
+        }
+        exclude = ['owner_comment']        
+        error_messages = {
+            "username":{
+                "required":"Bhai naam to daalo",
+                "max_length": "only add Chinna Swami of Chinna swami mutuswami vengopal iyer..."
+            }
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -7,13 +7,15 @@ from .models import Review
 
 def review(request):
     if request.method == "POST":
+        # can also update entries by passing "instance = existing_entry" to below ReviewForm (using Reviews.objects.get(pk=1)) 
         form = ReviewForm(request.POST)
         
         if form.is_valid():
-            review = Review(user_name= form.cleaned_data['username'],
-                            review_text= form.cleaned_data['review_text'],
-                            rating= form.cleaned_data['rating'])
-            review.save()
+            # review = Review(username= form.cleaned_data['username'],
+            #                 review_text= form.cleaned_data['review_text'],
+            #                 rating= form.cleaned_data['rating'])
+            # review.save()
+            form.save()     # since it is a ModelForm now
             return HttpResponseRedirect('/thanks')
     else:
         form = ReviewForm()
