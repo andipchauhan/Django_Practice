1f1ec60337631dbea7f8b069587590d165de1d7e
Andip Chauhan <chauhan2244andip@gmail.com>
2025-08-24 17:38:58 +0530

    Serving uploaded files using MEDIA_URL

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -4,19 +4,26 @@ from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import UserProfile
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 # Create your views here.
 
-# def store_file(file):
-#     with open("temp/image.jpg", "wb+") as dest:
-#         for chunk in file.chunks():
-#             dest.write(chunk)
 
 class CreateProfileView(CreateView):
     template_name = "profiles/create_profile.html"
     model = UserProfile
     fields = "__all__"
     success_url = '/profiles'
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = 'profiles/user_profiles.html'
+    context_object_name = 'profiles'
+
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 # class CreateProfileView(View):
 #     def get(self, request):
